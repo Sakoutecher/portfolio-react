31848f1dd97c4c8bf838d048b20ef487aa48909a
hcampos <hcampospro@gmail.com>
2022-11-16 15:18:31 +0100

    :speech_balloon: Update the presentation text in the description component

--- src/data/DataDescription.js
+++ src/data/DataDescription.js
@@ -1,4 +1,4 @@
-const description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis lorem pulvinar, eleifend ex vitae, varius erat. Proin commodo libero ligula, quis aliquam nulla fringilla sed. Quisque euismod orci ut ipsum mattis tempus. Curabitur metus lectus, congue nec nunc placerat, efficitur lacinia risus. Sed vitae lectus in sem pulvinar fringilla. Donec lacinia ut nisi vitae accumsan. Vestibulum pulvinar eget justo nec porta. Curabitur justo nibh, malesuada in porttitor ut, cursus nec nulla. Fusce lobortis ultrices tincidunt. Phasellus laoreet lacus sapien.'
+const description = "Hello everyone, i'm hcampos or hugo campos like you want 🙂. I'm a student who learning actually web development at Niort in France. I like to learn new things and to share my knowledges. I have been coding for 3 years from now, so i have done some projects, i hope you will enjoy it. You can see with my portfolio all of my school career and all the things i use for the development. If you have any question or if you want to talk with me no problem, see you in the contact section ☎️."
 
 const hobbies = [
   {
