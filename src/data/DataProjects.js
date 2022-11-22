5b04098ff113a637388a6a9733a9edca866d6f1b
hcampos <hcampospro@gmail.com>
2022-11-22 15:27:00 +0100

    :construction: Start the data file for the project

---
+++ src/data/DataProjects.js
@@ -0,0 +1,45 @@
+const ProjectsData = [
+    {
+        title: 'Portfolio',
+        techno : [
+            {
+                alt: 'react',
+                src: './techno-icons/React.png'
+            },
+
+            {
+                alt: 'styledcomponent',
+                src: './techno-icons/StyledComponent.png'
+            },
+
+            {
+                alt: 'js',
+                src: './techno-icons/JS.png'
+            },
+
+            {
+                alt: 'eslint',
+                src: './techno-icons/ESLint.png'
+            },
+
+            {
+                alt: 'prettier',
+                src: './techno-icons/Prettier.png'
+            }
+        ]
+    },
+
+    {
+        title: "Cuicuiteday's Website",
+        link: 'https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white',
+        star: '3',
+    },
+
+    {
+        title: 'Color guess game',
+        link: 'https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white',
+        star: '3',
+    },
+]
+
+export {ProjectsData}
