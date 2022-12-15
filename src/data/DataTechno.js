7cdb6363816abddd42023bb1647fafe5177d71ee
hcampos <hcampospro@gmail.com>
2022-12-15 15:31:15 +0100

    :bento: Add new technos (expressjs, socket.io)

--- src/data/DataTechno.js
+++ src/data/DataTechno.js
@@ -1,165 +1,182 @@
 const TechnoLinksFront = [
-    {
-        title: 'html',
-        link: 'https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white',
-        star: '3',
-    },
+  {
+    title: 'html',
+    link: 'https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white',
+    star: '3'
+  },
 
-    {
-        title: 'css',
-        link: 'https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white',
-        star: '3',
-    },
+  {
+    title: 'css',
+    link: 'https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white',
+    star: '3'
+  },
 
-    {
-        title: 'sass',
-        link: 'https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white',
-        star: '3',
-    },
+  {
+    title: 'sass',
+    link: 'https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white',
+    star: '3'
+  },
 
-    {
-        title: 'javascript',
-        link: 'https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E',
-        star: '2',
-    },
+  {
+    title: 'javascript',
+    link: 'https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E',
+    star: '2'
+  },
 
-    {
-        title: 'typescript',
-        link: 'https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white',
-        star: '1',
-    },
+  {
+    title: 'typescript',
+    link: 'https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white',
+    star: '1'
+  }
 ]
 
 const TechnoLinksBack = [
-    {
-        title: 'mariadb',
-        link: 'https://img.shields.io/badge/MariaDB-003545?style=for-the-badge&logo=mariadb&logoColor=white',
-        star: '2',
-    },
+  {
+    title: 'mariadb',
+    link: 'https://img.shields.io/badge/MariaDB-003545?style=for-the-badge&logo=mariadb&logoColor=white',
+    star: '2'
+  },
 
-    {
-        title: 'mysql',
-        link: 'https://img.shields.io/badge/MySQL-005C84?style=for-the-badge&logo=mysql&logoColor=white',
-        star: '2',
-    },
+  {
+    title: 'mysql',
+    link: 'https://img.shields.io/badge/MySQL-005C84?style=for-the-badge&logo=mysql&logoColor=white',
+    star: '2'
+  },
 
-    {
-        title: 'php',
-        link: 'https://img.shields.io/badge/PHP-777BB4?style=for-the-badge&logo=php&logoColor=white',
-        star: '3',
-    },
+  {
+    title: 'php',
+    link: 'https://img.shields.io/badge/PHP-777BB4?style=for-the-badge&logo=php&logoColor=white',
+    star: '3'
+  }
 ]
 
 const TechnoLinksFramework = [
-    {
-        title: 'apache',
-        link: 'https://img.shields.io/badge/Apache-D22128?style=for-the-badge&logo=Apache&logoColor=white',
-        star: '1',
-    },
+  {
+    title: 'apache',
+    link: 'https://img.shields.io/badge/Apache-D22128?style=for-the-badge&logo=Apache&logoColor=white',
+    star: '1'
+  },
 
-    {
-        title: 'chart.js',
-        link: 'https://img.shields.io/badge/Chart.js-FF6384?style=for-the-badge&logo=chartdotjs&logoColor=white',
-        star: '1',
-    },
+  {
+    title: 'chart.js',
+    link: 'https://img.shields.io/badge/Chart.js-FF6384?style=for-the-badge&logo=chartdotjs&logoColor=white',
+    star: '1'
+  },
 
-    {
-        title: 'fontawesome',
-        link: 'https://img.shields.io/badge/Font_Awesome-339AF0?style=for-the-badge&logo=fontawesome&logoColor=white',
-        star: '3',
-    },
+  {
+    title: 'fontawesome',
+    link: 'https://img.shields.io/badge/Font_Awesome-339AF0?style=for-the-badge&logo=fontawesome&logoColor=white',
+    star: '3'
+  },
 
-    {
-        title: 'githubpages',
-        link: 'https://img.shields.io/badge/GitHub%20Pages-222222?style=for-the-badge&logo=GitHub%20Pages&logoColor=white',
-        star: '2',
-    },
+  {
+    title: 'githubpages',
+    link: 'https://img.shields.io/badge/GitHub%20Pages-222222?style=for-the-badge&logo=GitHub%20Pages&logoColor=white',
+    star: '2'
+  },
 
-    {
-        title: 'jquery',
-        link: 'https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white',
-        star: '1',
-    },
+  {
+    title: 'jquery',
+    link: 'https://img.shields.io/badge/jQuery-0769AD?style=for-the-badge&logo=jquery&logoColor=white',
+    star: '1'
+  },
 
-    {
-        title: 'laragon',
-        link: 'https://img.shields.io/badge/Laragon-0E83CD?style=for-the-badge&logo=Laragon&logoColor=white',
-        star: '2',
-    },
+  {
+    title: 'laragon',
+    link: 'https://img.shields.io/badge/Laragon-0E83CD?style=for-the-badge&logo=Laragon&logoColor=white',
+    star: '2'
+  },
 
-    {
-        title: 'node.js',
-        link: 'https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white',
-        star: '2',
-    },
+  {
+    title: 'node.js',
+    link: 'https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white',
+    star: '2'
+  },
 
-    {
-        title: 'mocha',
-        link: 'https://img.shields.io/badge/Mocha-8D6748?style=for-the-badge&logo=Mocha&logoColor=white',
-        star: '1',
-    },
+  {
+    title: 'mocha',
+    link: 'https://img.shields.io/badge/Mocha-8D6748?style=for-the-badge&logo=Mocha&logoColor=white',
+    star: '1'
+  },
 
-    {
-        title: 'npm',
-        link: 'https://img.shields.io/badge/npm-CB3837?style=for-the-badge&logo=npm&logoColor=white',
-        star: '2',
-    },
+  {
+    title: 'npm',
+    link: 'https://img.shields.io/badge/npm-CB3837?style=for-the-badge&logo=npm&logoColor=white',
+    star: '2'
+  },
 
-    {
-        title: 'react',
-        link: 'https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB',
-        star: '2',
-    },
+  {
+    title: 'react',
+    link: 'https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB',
+    star: '2'
+  },
 
-    {
-        title: 'reactrouter',
-        link: 'https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white',
-        star: '2',
-    },
-   
-    {
-        title: 'xampp',
-        link: 'https://img.shields.io/badge/Xampp-F37623?style=for-the-badge&logo=xampp&logoColor=white',
-        star: '3',
-    },
+  {
+    title: 'reactrouter',
+    link: 'https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white',
+    star: '2'
+  },
 
-    {
-        title: 'yarn',
-        link: 'https://img.shields.io/badge/Yarn-2C8EBB?style=for-the-badge&logo=yarn&logoColor=white',
-        star: '2',
-    },
+  {
+    title: 'xampp',
+    link: 'https://img.shields.io/badge/Xampp-F37623?style=for-the-badge&logo=xampp&logoColor=white',
+    star: '3'
+  },
 
-    {
-        title: 'styled-component',
-        link: 'https://img.shields.io/badge/styled--components-DB7093?style=for-the-badge&logo=styled-components&logoColor=white',
-        star: '2',
-    },
+  {
+    title: 'yarn',
+    link: 'https://img.shields.io/badge/Yarn-2C8EBB?style=for-the-badge&logo=yarn&logoColor=white',
+    star: '2'
+  },
 
-    {
-        title: 'angular',
-        link: 'https://img.shields.io/badge/Angular-DD0031?style=for-the-badge&logo=angular&logoColor=white',
-        star: '1',
-    },
+  {
+    title: 'styled-component',
+    link: 'https://img.shields.io/badge/styled--components-DB7093?style=for-the-badge&logo=styled-components&logoColor=white',
+    star: '2'
+  },
 
-    {
-        title: 'react-native',
-        link: 'https://img.shields.io/badge/React_Native-20232A?style=for-the-badge&logo=react&logoColor=61DAFB',
-        star: '1',
-    },
+  {
+    title: 'angular',
+    link: 'https://img.shields.io/badge/Angular-DD0031?style=for-the-badge&logo=angular&logoColor=white',
+    star: '1'
+  },
+
+  {
+    title: 'react-native',
+    link: 'https://img.shields.io/badge/React_Native-20232A?style=for-the-badge&logo=react&logoColor=61DAFB',
+    star: '1'
+  },
+
+  {
+    title: 'expressjs',
+    link: 'https://img.shields.io/badge/Express.js-000000?style=for-the-badge&logo=express&logoColor=white',
+    star: '1'
+  },
+
+  {
+    title: 'socket.io',
+    link: 'https://img.shields.io/badge/Socket.io-010101?&style=for-the-badge&logo=Socket.io&logoColor=white',
+    star: '1'
+  }
 ]
 
 const TechnoLinksLanguages = [
-    {
-        title: 'french',
-        link: 'https://img.shields.io/badge/French-Native-3BC3A4?style=for-the-badge',
-        star: '3',
-    },
+  {
+    title: 'french',
+    link: 'https://img.shields.io/badge/French-Native-3BC3A4?style=for-the-badge',
+    star: '3'
+  },
 
-    {
-        title: 'english',
-        link: 'https://img.shields.io/badge/English-B2-3BC3A4?style=for-the-badge',
-        star: '3',
-    },
+  {
+    title: 'english',
+    link: 'https://img.shields.io/badge/English-B2-3BC3A4?style=for-the-badge',
+    star: '3'
+  }
 ]
 
-export {TechnoLinksLanguages, TechnoLinksFramework, TechnoLinksFront, TechnoLinksBack}
+export {
+  TechnoLinksLanguages,
+  TechnoLinksFramework,
+  TechnoLinksFront,
+  TechnoLinksBack
+}
